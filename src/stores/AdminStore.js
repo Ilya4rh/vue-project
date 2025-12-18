543f86e05744d7dbbfa3f24f4cf944bab4402dae
Ilya4rh <chernykn12ilya12@gmail.com>
2025-12-18 13:17:15 +0500

    Добавлен Pinia-store для хранения данных админа-заведения

---
+++ src/stores/AdminStore.js
@@ -0,0 +1,28 @@
+﻿import { defineStore } from 'pinia'
+
+export const adminStore = defineStore('admin', {
+    state: () => ({
+        dto: {
+            coffeeShopId: '',
+            login: '',
+            coffeeShopName: '',
+            city: '',
+            password: ''
+        }
+    }),
+
+    actions: {
+        update(payload) {
+            Object.assign(this.dto, payload)
+        },
+        reset() {
+            this.dto = {
+                coffeeShopId: '',
+                login: '',
+                coffeeShopName: '',
+                city: '',
+                password: ''
+            }
+        }
+    }
+})
