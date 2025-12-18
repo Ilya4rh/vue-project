8fcd03fa5677ea7d521a8247dcb864793f83c0fb
Ilya4rh <chernykn12ilya12@gmail.com>
2025-12-18 11:26:35 +0500

    Добавлен Pinia-store для хранения данных клиента

---
+++ src/stores/ClientStore.js
@@ -0,0 +1,24 @@
+﻿import { defineStore } from 'pinia'
+
+export const clientStore = defineStore('client', {
+    state: () => ({
+        dto: {
+            name: '',
+            phone: '',
+            code: ''
+        }
+    }),
+
+    actions: {
+        update(payload) {
+            Object.assign(this.dto, payload)
+        },
+        reset() {
+            this.dto = {
+                name: '',
+                phone: '',
+                code: ''
+            }
+        }
+    }
+})
