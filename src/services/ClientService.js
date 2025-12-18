ce230a3231fdb0a60285eba4125d25a9caf9d1f2
Ilya4rh <chernykn12ilya12@gmail.com>
2025-12-18 13:46:53 +0500

    Добавил клиент сервис с получением данных о текущем клиенте

---
+++ src/services/ClientService.js
@@ -0,0 +1,33 @@
+﻿import {clientStore} from "@/stores/ClientStore";
+
+export async function getCurrentUserInfo() {
+    const store = clientStore();
+    
+    try {
+        const token = store.dto.access_token;
+        
+        const response = await fetch(
+            `/api/v1/users/me`,
+            {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                }
+            }
+        )
+
+        if (!response.ok) {
+            console.log('Ошибка запроса')
+        }
+
+        const data = await response.json()
+
+        store.update({id: data.id, name: data.name});
+        
+        console.log(data)
+    } catch (error) {
+        console.error(error)
+    }
+}
